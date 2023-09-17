c7a9a521f50203528f72b1088f7ce3b266065d92
higgsboson20 <samtynanasbell@gmail.com>
2023-09-16 22:44:39 -0700

    Commit2

---
+++ Scripts.js
@@ -0,0 +1,13 @@
+var AWS = require('aws-sdk')
+
+function onClick(x){
+    switch(x){
+        case 1:
+            document.getElementById("Button1")
+            break;
+        case 2:
+        case 3:
+        default:
+            alert("arr with JS!!");
+    }
+}
